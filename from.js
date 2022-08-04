120c25651f5274cc6cfb320744d0ebfd648ed06f
Michel Ferreira <michel.ferreira.melo@gmail.com>
2022-08-04 15:03:14 -0300

    Add o método from

--- from.js
+++ from.js
@@ -5,6 +5,7 @@ const {numeros, ehPar} = require('./base');
 const copia1 = Array.from(numeros);
 console.log(...copia1);
 
-//é possível passar uma condição no momento da criação
+//é possível passar uma condição no momento da criação para mapear os elementos
 const copia2 = Array.from(numeros, (elemento) => elemento > 10);
 console.log(...copia2);
+//copia2 recebe um novo array com valores true caso elemento > 10, e false caso não sejam. 
